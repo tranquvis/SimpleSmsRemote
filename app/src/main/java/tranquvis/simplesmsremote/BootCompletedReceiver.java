e38272443edbcc9b13ee2af1141b3f224a03b3b6
tranquvis <kaltenleitner.andreas@gmail.com>
2016-09-01 19:51:48 +0200

    developing

--- app/src/main/java/tranquvis/simplesmsremote/BootCompletedReceiver.java
+++ app/src/main/java/tranquvis/simplesmsremote/BootCompletedReceiver.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.widget.Toast;
 
 import tranquvis.simplesmsremote.Data.DataManager;
 import tranquvis.simplesmsremote.ReceiverService.SMSReceiverService;
@@ -16,11 +15,16 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         try {
-            Log.i("simplesmsremote", "boot completed");
+            Log.i("bootCompletedReceiver", "broadcast received");
             DataManager.LoadUserData(context);
-            if(DataManager.getUserData().getUserSettings().isStartReceiverOnSystemStart())
+            if(DataManager.getUserData().getUserSettings().isStartReceiverOnSystemStart()) {
                 SMSReceiverService.start(context);
+                Log.i("receiver", "service started successful");
+            }
+            Log.i("bootCompletedReceiver", "service not started (pref not enabled)");
         } catch (Exception e) {
+            Log.e("bootCompletedReceiver", "failed to start service");
+            e.printStackTrace();
             MyNotificationManager.getInstance(context).notifyStartReceiverAfterBootFailed();
         }
     }
