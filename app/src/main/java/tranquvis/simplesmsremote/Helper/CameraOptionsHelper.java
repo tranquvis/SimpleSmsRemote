434e3bfb0c99f13c87bf87e298edb74caa523cca
Bitbucket WebSSH JavaEEWebApp <kaltenleitner.andreas@gmail.com>
2016-10-24 18:01:54 +0200

    camera module update

--- app/src/main/java/tranquvis/simplesmsremote/Helper/CameraOptionsHelper.java
+++ app/src/main/java/tranquvis/simplesmsremote/Helper/CameraOptionsHelper.java
@@ -1,5 +1,7 @@
 package tranquvis.simplesmsremote.Helper;
 
+import org.intellij.lang.annotations.Language;
+
 /**
  * Created by Andreas Kaltenleitner on 24.10.2016.
  */
@@ -8,9 +10,9 @@ public class CameraOptionsHelper
 {
     public enum TakePhotoOptions
     {
-        FRONT_CAMERA("front camera", "(cam(era)?\\s*front)|(front\\s*cam(era)?)"),
-        BACK_CAMERA("back camera", "(cam(era)?\\s*back)|(back\\s*cam(era)?)"),
-        CAMERA_INDEX("camera index", "camera\\s*\\d{1,2}"),
+        FRONT_CAMERA("front camera", "(cam(era)?\\s+front)|(front\\s+cam(era)?)"),
+        BACK_CAMERA("back camera", "(cam(era)?\\s+back)|(back\\s+cam(era)?)"),
+        CAMERA_INDEX("camera index", "cam(era)?\\s+\\d{1,2}"),
         FLASH_ON,
         FLASH_OFF,
         FLASH_AUTO;
@@ -23,8 +25,9 @@ public class CameraOptionsHelper
          * @param name option name
          * @param matcher regular expression, which defines which strings match this option
          */
-        TakePhotoOptions(String name, String matcher)
+        TakePhotoOptions(String name, @Language("RegExp") String matcher)
         {
+            String s = "";
             this.name = name;
             this.matcher = matcher;
         }
