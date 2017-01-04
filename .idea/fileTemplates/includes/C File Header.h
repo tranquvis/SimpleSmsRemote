1e56a2be9555c1aa33cc9b9d75e45b948373ff9d
Andreas Kaltenleitner <kaltenleitner.andreas@gmail.com>
2017-01-04 11:22:57 +0100

    update

---
+++ .idea/fileTemplates/includes/C File Header.h
@@ -0,0 +1,9 @@
+#if ($HEADER_COMMENTS)
+//
+// Created by Kaltenleitner Andreas on ${DATE}.
+#if ($ORGANIZATION_NAME && $ORGANIZATION_NAME != "")
+// Copyright (c) $YEAR ${ORGANIZATION_NAME}#if (!$ORGANIZATION_NAME.endsWith(".")).#end All rights reserved.
+#end
+//
+#end
+
