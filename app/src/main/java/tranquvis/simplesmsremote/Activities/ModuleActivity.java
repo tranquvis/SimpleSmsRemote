955235460dfba45b73ff8e28789fc88f5299053c
Pieter <pieter@pfiers.net>
2021-08-21 12:39:37 +0200

    Remove some stray imports

--- app/src/main/java/tranquvis/simplesmsremote/Activities/ModuleActivity.java
+++ app/src/main/java/tranquvis/simplesmsremote/Activities/ModuleActivity.java
@@ -14,7 +14,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.Spanned;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewStub;
